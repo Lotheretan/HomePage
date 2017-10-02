06e0592bba2b02b6106d5c5120e6879ef5d31a06
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2017-10-02 10:47:57 +0200

    Controllers
    Création des controllers

---
+++ app/controllers/MoteurController.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers;
+ /**
+ * Controller MoteurController
+ **/
+class MoteurController extends ControllerBase{
+
+	public function index(){}
+
+}
---
+++ app/controllers/OptionController.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers;
+ /**
+ * Controller OptionController
+ **/
+class OptionController extends ControllerBase{
+
+	public function index(){}
+
+}

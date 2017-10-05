35da6c102bd252954e67caeee586d2c809001c5e
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2017-10-05 12:03:00 +0200

    Début Controller Site

--- app/cache/annotations/SiteController.php-9e0a18bf.annotations.php
+++ app/cache/annotations/SiteController.php-9e0a18bf.annotations.php
@@ -3,6 +3,7 @@
 return array(
   '#namespace' => 'controllers',
   '#uses' => array (
+  'DAO' => 'micro\\orm\\DAO',
 ),
   '#traitMethodOverrides' => array (
   'controllers\\SiteController' => 
--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -1,10 +1,38 @@
 <?php
 namespace controllers;
+ use micro\orm\DAO;
+use Ajax\JsUtils;
+
  /**
  * Controller SiteController
+ * @property JsUtils $jquery
  **/
 class SiteController extends ControllerBase{
 
-	public function index(){}
+	public function index(){
+		$semantic=$this->jquery->semantic();
+		$semantic->htmlHeader("header",1,"Liste des sites");
+		$this->jquery->compile($this->view);
+		$this->loadView("site/index.html");
+		$site=DAO::getAll("models\Site");
+		$semantic=$this->jquery->semantic();
+		$table=$semantic->dataTable("site", "models\Site", $site);
+		$table->setFields(["id","nom","latitude","longitude"]);
+		$table->setCaptions(["Identifiant", "Nom","Latitude","Longitude"]);
+		$table->addEditButton();
+		$table->addDeleteButton();
+		$table->setUrls(["","SiteController/UpdateSite","SiteController/DeleteSite"]);
+		$table->setTargetSelector("#site");
+		echo $table->compile($this->jquery);
+		echo $this->jquery->compile();
+	}
+	public function UpdateSite(){
+		$semantic=$this->jquery->semantic();
+		$this->jquery->compile($this->view);
+		$this->loadView("site/Update.html");
+	}
+	public function DeleteSite(){
+		
+	}
 
 }

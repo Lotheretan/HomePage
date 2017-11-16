1002dd6e2ced71b229cff0f861c3296e3c367373
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2017-11-16 10:55:15 +0100

    Rafraichissement OK

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -4,7 +4,6 @@ use Ajax\JsUtils;
 use micro\orm\DAO;
 use Ajax\semantic\html\collections\form\HtmlFormInput;
 use libraries;
-use Ajax\semantic\html\collections\form\HtmlFormField;
  /**
  * Controller Main
  *@property JsUtils $jquery
@@ -14,21 +13,27 @@ class Main extends ControllerBase{
 	public function index(){
 		$semantic=$this->jquery->semantic();
 		$semantic->htmlHeader("header",1,"Accueil du site");
-		if(!isset($_SESSION["user"]))
-		{
-    		$bts=$semantic->htmlButtonGroups("Buttons",["Connexion"]);
-    		$bts->getOnClick("Main/connexion/","#divUsers");
-		}
-    	else{
-    	    $bt=$semantic->htmlButton("bt","Utilisateurs");
-    	    $bt->asLink("UtilisateurController");
-    	    $bt2=$semantic->htmlButton("bt2","Sites");
-    	    $bt2->asLink("SiteController");
-    	    $bt3=$semantic->htmlButtonGroups("Buttons",["Deconnexion"]);
-    	    $bt3->getOnClick("Main/disconnect/","#divUsers");
-    	}		
+		$this->getButtons();
 		$this->jquery->compile($this->view);
 		$this->loadView("index.html",array("divUsers"=>libraries\Auth::getInfoUser()));
+	}
+	
+	public function getButtons(){
+		$semantic=$this->jquery->semantic();
+		
+		if(!isset($_SESSION["user"]))
+		{
+			$bts=$semantic->htmlButtonGroups("Buttons",["Connexion"]);
+			$bts->getOnClick("Main/connexion/","#divUsers");
+		}
+		else{
+			$bt=$semantic->htmlButton("bt","Utilisateurs");
+			$bt->asLink("UtilisateurController");
+			$bt2=$semantic->htmlButton("bt2","Sites");
+			$bt2->asLink("SiteController");
+			$bt3=$semantic->htmlButtonGroups("Buttons",["Deconnexion"]);
+			$bt3->getOnClick("Main/disconnect/","#divUsers");
+		}
 	}
 	
 	public function connexion(){
@@ -44,22 +49,30 @@ class Main extends ControllerBase{
 	public function connect(){
 	    $user=DAO::getOne("models\Utilisateur","login='".$_POST['Login']."'");
 	    if(isset($user)){
-	        if($user->getPassword()==$_POST["Password"]){
+	        if($user->getPassword()===$_POST["Password"]){
 	            $_SESSION["user"]=$user;
 	            echo "Bienvenue utilisateur : ".$user->getLogin();
+	            $this->jquery->get("Main/index","body");
+	            echo $this->jquery->compile($this->view);
+	           
 		}else{
 			echo "Mauvais identifiant et/ou mot de passe ! ";
 		}
 	    }else {getInfoUser();}
+	    
 	}
 	
 	public function disconnect(){
 		// On détruit les variables de notre session
-		session_unset ();
+		session_unset();
 		// On détruit notre session
+		
 		session_destroy ();
 		
-		$this->jquery->get("Main/connexion","#divUsers");
+		$this->jquery->get("Main/index","body");
+		echo $this->jquery->compile($this->view);
+		
+		
 
 		
 	}
